0ce4f1b9f26a4e42dda31138b28127c77116f09c
Muhammad Luqman <luqman@cinergiedigital.com>
2024-07-18 00:29:31 +0500

    Get Api for category

--- app/api/(dashboard)/categories/route.ts
+++ app/api/(dashboard)/categories/route.ts
@@ -8,9 +8,35 @@ import { url } from 'inspector';
 
 export const GET = async (request: Request) => {
     try {
+        const { searchParams } = new URL(request.url);
+        const userId = searchParams.get('userId');
+        if (!userId || !Types.ObjectId.isValid(userId)) {
+            return new NextResponse(JSON.stringify({ message: "Invalid or missing user id" }), { 'status': 400 })
+        }
+
+        await connect()
+
+        const user = await User.findById(userId)
+
+        if (!user) {
+            return new NextResponse(JSON.stringify({ message: "User not found in the database" }), { 'status': 404 })
+        }
+
+        const categories = await Category.find({ userId: new Types.ObjectId(userId) })
+
+        return new NextResponse(JSON.stringify(categories), { 'status': 200 })
 
     }
     catch (err: any) {
-        return new NextResponse("Error getting the category", err.message)
+        return new NextResponse("Error getting the category" + err.message, { status: 500 })
+    }
+}
+
+export const POST = async (request: Request) => {
+    try {
+
+    }
+    catch (err: any) {
+        return new NextResponse("Error creating the category" + err.message, { status: 500 })
     }
 }
